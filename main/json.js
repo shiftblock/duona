6a7a4c75aaab1001ba4a733122cde546f27fb53d
shiftblock <641777872@qq.com>
2017-06-27 09:27:46 +0800

    duona shop website

---
+++ main/json.js
@@ -0,0 +1,132 @@
+var a=
+{
+  "ret": true,
+  "errorCode": 0,
+  "errorMsg": "",
+  "data": [
+    {
+      "title" : "锦霸去渍霸薰衣草洗衣液2kg",
+      "img": "./img/sale1.jpg",
+      "prace":"￥10.00"
+      
+    },
+    {
+      "title" : "汰渍去渍霸薰衣草洗衣粉2kg",
+      "img": "./img/sale2.jpg",
+      "prace":"￥19.90"
+    },
+    {
+      "title" : "古凌山超厚苏打饼干1kg",
+      "img": "./img/sale3.jpg",
+      "prace":"￥9.90"
+    },
+    {
+      "title" : "雨森春意盎然卫生纸2000g",
+      "img": "./img/sale4.jpg",
+      "prace":"￥19.80"
+    },
+    {
+      "title" : "周氏秘制天然蜂蜜500g",
+      "img": "./img/sale5.jpg",
+      "prace":"￥27.80"
+    },
+    {
+      "title" : "雨森宝贝狗启封湿巾10片",
+      "img": "./img/sale6.jpg",
+      "prace":"￥1.00"
+    },
+    {
+      "title" : "锦霸去渍霸薰衣草洗衣液2kg",
+      "img": "./img/sale1.jpg",
+      "prace":"￥10.00"
+      
+    },
+    {
+      "title" : "汰渍去渍霸薰衣草洗衣粉2kg",
+      "img": "./img/sale2.jpg",
+      "prace":"￥19.90"
+    },
+    {
+      "title" : "古凌山超厚苏打饼干1kg",
+      "img": "./img/sale3.jpg",
+      "prace":"￥9.90"
+    },
+    {
+      "title" : "雨森春意盎然卫生纸2000g",
+      "img": "./img/sale4.jpg",
+      "prace":"￥19.80"
+    },
+    {
+      "title" : "周氏秘制天然蜂蜜500g",
+      "img": "./img/sale5.jpg",
+      "prace":"￥27.80"
+    },
+    {
+      "title" : "雨森宝贝狗启封湿巾10片",
+      "img": "./img/sale6.jpg",
+      "prace":"￥1.00"
+    },
+    {
+      "title" : "锦霸去渍霸薰衣草洗衣液2kg",
+      "img": "./img/sale1.jpg",
+      "prace":"￥10.00"
+      
+    },
+    {
+      "title" : "汰渍去渍霸薰衣草洗衣粉2kg",
+      "img": "./img/sale2.jpg",
+      "prace":"￥19.90"
+    },
+    {
+      "title" : "古凌山超厚苏打饼干1kg",
+      "img": "./img/sale3.jpg",
+      "prace":"￥9.90"
+    },
+    {
+      "title" : "雨森春意盎然卫生纸2000g",
+      "img": "./img/sale4.jpg",
+      "prace":"￥19.80"
+    },
+    {
+      "title" : "周氏秘制天然蜂蜜500g",
+      "img": "./img/sale5.jpg",
+      "prace":"￥27.80"
+    },
+    {
+      "title" : "雨森宝贝狗启封湿巾10片",
+      "img": "./img/sale6.jpg",
+      "prace":"￥1.00"
+    },
+    {
+      "title" : "锦霸去渍霸薰衣草洗衣液2kg",
+      "img": "./img/sale1.jpg",
+      "prace":"￥10.00"
+      
+    },
+    {
+      "title" : "汰渍去渍霸薰衣草洗衣粉2kg",
+      "img": "./img/sale2.jpg",
+      "prace":"￥19.90"
+    },
+    {
+      "title" : "古凌山超厚苏打饼干1kg",
+      "img": "./img/sale3.jpg",
+      "prace":"￥9.90"
+    },
+    {
+      "title" : "雨森春意盎然卫生纸2000g",
+      "img": "./img/sale4.jpg",
+      "prace":"￥19.80"
+    },
+    {
+      "title" : "周氏秘制天然蜂蜜500g",
+      "img": "./img/sale5.jpg",
+      "prace":"￥27.80"
+    },
+    {
+      "title" : "雨森宝贝狗启封湿巾10片",
+      "img": "./img/sale6.jpg",
+      "prace":"￥1.00"
+    }
+  ]
+}
